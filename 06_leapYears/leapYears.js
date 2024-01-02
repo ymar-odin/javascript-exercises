b7a9873c5042e7deb4c718c0e22da8a470d3f283
ymar-odin <150017531+ymar-odin@users.noreply.github.com>
2024-01-02 10:20:51 +0200

    Complete exercise 6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,16 @@
-const leapYears = function() {
+const leapYears = function(year) {
+
+    if(year % 4 === 0 && year % 100 == 0) {
+        if (year % 400 === 0) {
+            return true;
+        } else {
+            return false;
+        }
+    } else if (year % 4 === 0 && year % 100 !== 0){
+        return true;
+    } else {
+        return false;
+    }
 
 };
 
