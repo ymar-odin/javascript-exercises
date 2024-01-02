08e3223bec9478459078d93deb6a94070543d145
ymar-odin <150017531+ymar-odin@users.noreply.github.com>
2024-01-02 11:08:29 +0200

    Complete exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  let convTemp = (temp - 32) * (5 / 9);
+  return +convTemp.toFixed(1);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  let convTemp = (temp * (9/5)) + 32;
+  return +convTemp.toFixed(1);
 };
 
 // Do not edit below this line
